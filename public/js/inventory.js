f55e6890dae90c249780894741f438868fdff8cb
Gaurav Viramgami <viramgami.g@iitgn.ac.in>
2021-05-28 18:46:41 +0530

    Final Changes

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -5,46 +5,56 @@ var jqxhr = $.ajax({
   })
   .then(function(doRef) {
     var data = doRef["data"];
-    var items = data.split("&&&&***&&&&");
-    for(var i = 0; i < items.length; i++) {
-      items[i] = items[i].split("&&&**&&&");
-    }
-    //console.log(items);
+    try {
+      var items = data.split("&&&&***&&&&");
+      for(var i = 0; i < items.length; i++) {
+        items[i] = items[i].split("&&&**&&&");
+      }
+      //console.log(items);
 
-    for (var i = 0; i < items.length; i++) {
-      //var codeID = "code" + (i+1).toString();
-      //var pointsID = "points" + (i+1).toString();
-      var tbody = document.getElementById("tbody");
-      var row = document.createElement("tr");
-      var cell1 = document.createElement("th");
-      var cell2 = document.createElement("td");
-      var cell3 = document.createElement("td");
-      var cell4 = document.createElement("td");
-      var cell5 = document.createElement("td");
-      var cell1Text = document.createTextNode(items[i][0]);
-      var cell2Img = document.createElement("img");
-      cell2Img.src = items[i][1];
-      var cell3Text = document.createTextNode(items[i][2]);
-      var cell4Text = document.createTextNode(items[i][3]);
-      var cell5Link = document.createElement("a");
-      var cell5Text = document.createTextNode("Link");
-      cell5Link.appendChild(cell5Text);
-      cell5Link.href = items[i][4];
-      cell5Link.target = "blank_";
-      cell1.appendChild(cell1Text);
-      cell2.appendChild(cell2Img);
-      cell3.appendChild(cell3Text);
-      cell4.appendChild(cell4Text);
-      cell5.appendChild(cell5Link);
-      row.appendChild(cell1);
-      row.appendChild(cell2);
-      row.appendChild(cell3);
-      row.appendChild(cell4);
-      row.appendChild(cell5);
-      tbody.appendChild(row);
-      //document.getElementById(codeID).innerHTML = CAs[i][0];
-      //document.getElementById(pointsID).innerHTML = CAs[i][1];
-    }
+      for (var i = 0; i < items.length; i++) {
+        //var codeID = "code" + (i+1).toString();
+        //var pointsID = "points" + (i+1).toString();
+        var tbody = document.getElementById("tbody");
+        var row = document.createElement("tr");
+        var cell1 = document.createElement("th");
+        var cell2 = document.createElement("td");
+        var cell3 = document.createElement("td");
+        var cell4 = document.createElement("td");
+        var cell5 = document.createElement("td");
+        var cell1Text = document.createTextNode(items[i][0]);
+        var cell2Img = document.createElement("img");
+        cell2Img.src = items[i][1];
+        var cell3Text = document.createTextNode(items[i][2]);
+        var cell4Text = document.createTextNode(items[i][3]);
+        var cell5Link = document.createElement("a");
+        var cell5Text = document.createTextNode("Link");
+        cell5Link.appendChild(cell5Text);
+        cell5Link.href = items[i][4];
+        cell5Link.target = "blank_";
+        cell1.appendChild(cell1Text);
+        cell2.appendChild(cell2Img);
+        cell3.appendChild(cell3Text);
+        cell4.appendChild(cell4Text);
+        cell5.appendChild(cell5Link);
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
+        row.appendChild(cell4);
+        row.appendChild(cell5);
+        tbody.appendChild(row);
+        //document.getElementById(codeID).innerHTML = CAs[i][0];
+        //document.getElementById(pointsID).innerHTML = CAs[i][1];
+      }
+  } catch(err) {
+    var tbody = document.getElementById("tbody");
+    var row = document.createElement("tr");
+    var cell1 = document.createElement("td");
+    var cell1Text = document.createTextNode("Will be updated soon!");
+    cell1.appendChild(cell1Text);
+    row.appendChild(cell1);
+    tbody.appendChild(row);
+  }
 
     document.getElementById("loading").style.display = "none";
     document.getElementById("main").style.display = "block";
