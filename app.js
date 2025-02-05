6247e15221c4764d4966e66173d8fe1ff4cb4391
Nikhilesh <nikhilesh.myanapuri@iitgn.ac.in>
2025-02-05 18:59:54 +0530

    updated TL team and location

--- app.js
+++ app.js
@@ -1,48 +1,96 @@
 require('dotenv').config()
 
+
+
 //const cors = require('cors');
 
+
+
 var express = require("express"),
+
   app = express(),
+
   methodOverride = require("method-override"),
+
   bodyParser = require("body-parser");
+
 const cookieParser = require('cookie-parser');
 
+
+
 // const passport = require('passport');
+
 // const cookieSession = require('cookie-session')
+
 // require('./passport-setup');
+
 //
+
 // app.use(cookieSession({
+
 //     name: 'tuto-session',
+
 //     keys: ['key1', 'key2']
+
 //   }))
+
+
 
 //app.use(cors());
 
+
+
 // app.use(passport.initialize());
+
 // app.use(passport.session());
 
+
+
 app.use(express.json());
+
 app.use(cookieParser());
+
 app.set("view engine", "ejs")
-app.use(express.static("public"))
+
+app.use(express.static("/tinkerers-lab/public"))
+
 app.use(bodyParser.urlencoded({
+
   extended: true
+
 }))
+
 app.use(methodOverride("_method"))
 
+
+
 app.use(function (req, res, next) {
+
 res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
 res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
+
 res.setHeader('Access-Control-Allow-Credentials', true);
+
 next();
+
 })
+
+
 
 var indexRoutes = require("./routes/index")
-app.use("/", indexRoutes)
 
-// Server http://localhost:3000/
+app.use("/tinkerers-lab", indexRoutes)
+
+
+
+// Server http://localhost:3000/tinkerers-lab
+
 app.listen(process.env.PORT || 3000, function () {
+
   console.log("app is running...")
+
 })
+
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 var express = require("express")
 var router = express.Router()
 // var passport = require("passport")
@@ -6,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 // Google Auth
 const {OAuth2Client} = require('google-auth-library');
-const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_ID = '229423012134-u4dv6s53mpllopu3ig79o4njqr4cr2nl.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 
 router.use(express.json());
@@ -16,9 +15,16 @@ router.get("/", function (req, res) {
   res.render("index");
 })
 
+router.get("/index.html", function (req, res) {
+  res.redirect("/tinkerers-lab");
+})
+
+router.get("/about.html", function (req, res) {
+  res.redirect("/tinkerers-lab");
+})
+
 router.post('/login', (req,res)=>{
     let token = req.body.token;
-
     async function verify() {
         const ticket = await client.verifyIdToken({
             idToken: token,
@@ -33,45 +39,45 @@ router.post('/login', (req,res)=>{
           res.send('success')
       })
       .catch(console.error);
-
 })
 
 router.get('/logout', (req, res)=>{
     res.clearCookie('session-token');
-    res.redirect('/booking')
-
+    res.redirect('/tinkerers-lab/booking')
 })
 
-router.get("/booking", checkAuthenticated, function (req, res) {
-
-  if (req.user) {
-    // console.log(req.user);
-    // console.log("Logged in");
-    // console.log(req.user.emails[0].value);
-    res.render("booking", {loggedIn: true, email: req.user.email});
-    // res.render("booking", {loggedIn: false});
-  } else {
-    // console.log("Not logged in");
-    res.render("booking", {loggedIn: false});
-  }
-  // res.render("booking");
+router.get("/booking", function (req, res) {
+  // Pass default variables for the booking view.
+  res.render("booking", { loggedIn: false, email: "-" });
 })
 
-router.get("/booking_withoutLogin", function (req, res) {
+// router.get("/booking", checkAuthenticated, function (req, res) {
+//   if (req.user) {
+//     // console.log(req.user);
+//     // console.log("Logged in");
+//     // console.log(req.user.emails[0].value);
+//     res.render("booking", {loggedIn: true, email: req.user.email});
+//     // res.render("booking", {loggedIn: false});
+//   } else {
+//     // console.log("Not logged in");
+//     res.render("booking", {loggedIn: false});
+//   }
+//   // res.render("booking");
+// })
 
-  // if (req.user) {
-  //   // console.log("Logged in");
-  //   // console.log(req.user.emails[0].value);
-  //   res.render("booking", {loggedIn: true, email: req.user.emails[0].value});
-  // } else {
-    // console.log("Not logged in");
-    res.render("booking", {loggedIn: false});
-  // }
-  // res.render("booking");
-})
+// router.get("/booking-login", function (req, res) {
+//   // if (req.user) {
+//   //   // console.log("Logged in");
+//   //   // console.log(req.user.emails[0].value);
+//   //   res.render("booking", {loggedIn: true, email: req.user.emails[0].value});
+//   // } else {
+//     // console.log("Not logged in");
+//     res.render("booking", {loggedIn: false});
+//   // }
+//   // res.render("booking");
+// })
 
-router.get("/mehtaLaser_form", checkAuthenticated, function (req, res) {
-
+router.get("/mehtaLaser-form", checkAuthenticated, function (req, res) {
   if (req.user) {
     // console.log("Logged in");
     // console.log(req.user.emails[0].value);
@@ -80,11 +86,9 @@ router.get("/mehtaLaser_form", checkAuthenticated, function (req, res) {
     // console.log("Not logged in");
     res.render("booking_forms/mehtaLaser_form", {loggedIn: false, email: "-"});
   }
-
 })
 
-router.get("/gccLaser_form", checkAuthenticated, function (req, res) {
-
+router.get("/gccLaser-form", checkAuthenticated, function (req, res) {
   if (req.user) {
     // console.log("Logged in");
     // console.log(req.user.emails[0].value);
@@ -93,11 +97,9 @@ router.get("/gccLaser_form", checkAuthenticated, function (req, res) {
     // console.log("Not logged in");
     res.render("booking_forms/gccLaser_form", {loggedIn: false, email: "-"});
   }
-
 })
 
-router.get("/3dprinter_form", checkAuthenticated, function (req, res) {
-
+router.get("/3dprinter-form", checkAuthenticated, function (req, res) {
   if (req.user) {
     // console.log("Logged in");
     // console.log(req.user.emails[0].value);
@@ -106,11 +108,9 @@ router.get("/3dprinter_form", checkAuthenticated, function (req, res) {
     // console.log("Not logged in");
     res.render("booking_forms/3dprinter_form", {loggedIn: false, email: "-"});
   }
-
 })
 
-router.get("/rolandVinylCutter_form", checkAuthenticated, function (req, res) {
-
+router.get("/rolandVinylCutter-form", checkAuthenticated, function (req, res) {
   if (req.user) {
     // console.log("Logged in");
     // console.log(req.user.emails[0].value);
@@ -119,61 +119,60 @@ router.get("/rolandVinylCutter_form", checkAuthenticated, function (req, res) {
     // console.log("Not logged in");
     res.render("booking_forms/rolandVinylCutter_form", {loggedIn: false, email: "-"});
   }
-
 })
 
-router.get("/mehtaLaser_calendar", checkAuthenticated, function (req, res) {
-
-  if (req.user) {
-    // console.log("Logged in");
-    // console.log(req.user.emails[0].value);
-    res.render("calendars/mehtaLaser_calendar", {loggedIn: true, email: req.user.email});
-  } else {
-    // console.log("Not logged in");
-    res.render("calendars/mehtaLaser_calendar", {loggedIn: false});
-  }
-
+router.get("/mehtaLaser-calendar", function (req, res) {
+    res.render("calendars/mehtaLaser_calendar");
 })
 
-router.get("/gccLaser_calendar", checkAuthenticated, function (req, res) {
-
-  if (req.user) {
-    // console.log("Logged in");
-    // console.log(req.user.emails[0].value);
-    res.render("calendars/gccLaser_calendar", {loggedIn: true, email: req.user.email});
-  } else {
-    // console.log("Not logged in");
-    res.render("calendars/gccLaser_calendar", {loggedIn: false});
-  }
-
+router.get("/3dprinter-calendar", function (req, res) {
+  res.render("calendars/3dprinter_calendar");
 })
 
-router.get("/3dprinter_calendar", checkAuthenticated, function (req, res) {
 
-  if (req.user) {
-    // console.log("Logged in");
-    // console.log(req.user.emails[0].value);
-    res.render("calendars/3dprinter_calendar", {loggedIn: true, email: req.user.email});
-  } else {
-    // console.log("Not logged in");
-    res.render("calendars/3dprinter_calendar", {loggedIn: false});
-  }
+// router.get("/mehtaLaser-calendar", checkAuthenticated, function (req, res) {
+//   if (req.user) {
+//     // console.log("Logged in");
+//     // console.log(req.user.emails[0].value);
+//     res.render("calendars/mehtaLaser_calendar", {loggedIn: true, email: req.user.email});
+//   } else {
+//     // console.log("Not logged in");
+//     res.render("calendars/mehtaLaser_calendar", {loggedIn: false});
+//   }
+// })
 
-})
+// router.get("/gccLaser-calendar", checkAuthenticated, function (req, res) {
+//   if (req.user) {
+//     // console.log("Logged in");
+//     // console.log(req.user.emails[0].value);
+//     res.render("calendars/gccLaser_calendar", {loggedIn: true, email: req.user.email});
+//   } else {
+//     // console.log("Not logged in");
+//     res.render("calendars/gccLaser_calendar", {loggedIn: false});
+//   }
+// })
 
-router.get("/rolandVinylCutter_calendar", checkAuthenticated, function (req, res) {
+// router.get("/3dprinter-calendar", checkAuthenticated, function (req, res) {
+//   if (req.user) {
+//     // console.log("Logged in");
+//     // console.log(req.user.emails[0].value);
+//     res.render("calendars/3dprinter_calendar", {loggedIn: true, email: req.user.email});
+//   } else {
+//     // console.log("Not logged in");
+//     res.render("calendars/3dprinter_calendar", {loggedIn: false});
+//   }
+// })
 
-  if (req.user) {
-    // console.log("Logged in");
-    // console.log(req.user.emails[0].value);
-    res.render("calendars/rolandVinylCutter_calendar", {loggedIn: true, email: req.user.email});
-  } else {
-    // console.log("Not logged in");
-    res.render("calendars/rolandVinylCutter_calendar", {loggedIn: false});
-  }
-
-})
-
+// router.get("/rolandVinylCutter-calendar", checkAuthenticated, function (req, res) {
+//   if (req.user) {
+//     // console.log("Logged in");
+//     // console.log(req.user.emails[0].value);
+//     res.render("calendars/rolandVinylCutter_calendar", {loggedIn: true, email: req.user.email});
+//   } else {
+//     // console.log("Not logged in");
+//     res.render("calendars/rolandVinylCutter_calendar", {loggedIn: false});
+//   }
+// })
 
 router.get("/events", function (req, res) {
   res.render("events");
@@ -183,8 +182,38 @@ router.get("/blogs", function (req, res) {
   res.render("blogs");
 })
 
-router.get("/inventory", function (req, res) {
-  res.render("inventory");
+router.get('/logout-inventory', (req, res)=>{
+  res.clearCookie('session-token');
+  res.redirect('tinkerers-lab/inventory')
+
+})
+
+router.get("/inventory", checkAuthenticatedInventory, function (req, res) {
+
+  if (req.user) {
+    // console.log(req.user);
+    // console.log("Logged in");
+    // console.log(req.user.emails[0].value);
+    res.render("inventory", {loggedIn: true, email: req.user.email});
+    // res.render("booking", {loggedIn: false});
+  } else {
+    // console.log("Not logged in");
+    res.render("inventory", {loggedIn: false, email: "-"});
+  }
+  // res.render("booking");
+})
+
+router.get("/inventory-login", function (req, res) {
+
+// if (req.user) {
+//   // console.log("Logged in");
+//   // console.log(req.user.emails[0].value);
+//   res.render("booking", {loggedIn: true, email: req.user.emails[0].value});
+// } else {
+  // console.log("Not logged in");
+  res.render("inventory", {loggedIn: false, email: "-"});
+// }
+// res.render("booking");
 })
 
 router.get("/about", function (req, res) {
@@ -196,24 +225,37 @@ router.get("/contact", function (req, res) {
 })
 
 // router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
 //
+
 // router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/booking' }),
+
 //   function(req, res) {
+
 //     // Successful authentication, redirect home.
+
 //     res.redirect('/booking');
+
 //   }
+
 // );
 
+
+
 // router.get('/logout', (req, res) => {
+
 //     req.session = null;
+
 //     req.logout();
+
 //     res.redirect('/booking');
+
 // })
 
+
+
 function checkAuthenticated(req, res, next){
-
     let token = req.cookies['session-token'];
-
     let user = {};
     async function verify() {
         const ticket = await client.verifyIdToken({
@@ -231,9 +273,35 @@ function checkAuthenticated(req, res, next){
           next();
       })
       .catch(err=>{
-          res.redirect('/booking_withoutLogin')
+          res.redirect('/tinkerers-lab/booking-login')
       })
+}
+
+function checkAuthenticatedInventory(req, res, next){
+
+  let token = req.cookies['session-token'];
+
+  let user = {};
+  async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      });
+      const payload = ticket.getPayload();
+      user.name = payload.name;
+      user.email = payload.email;
+      user.picture = payload.picture;
+    }
+    verify()
+    .then(()=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>{
+        res.redirect('/tinkerers-lab/inventory-login')
+    })
 
 }
 
 module.exports = router
+
